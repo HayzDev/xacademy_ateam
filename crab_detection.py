f329e21bfedf99f03e4c3ae5cb936f2d8629c8ce
Hayz <xanderbuhr714@gmail.com>
2025-12-14 15:30:01 -0800

    feat: Add dataset mode for image annotation in `crab_detection.py`, new COCO annotation file, and sample crab images.

--- crab_detection.py
+++ crab_detection.py
@@ -1,15 +1,66 @@
 """Detects crabs in images"""
 
+import json
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+import matplotlib.pyplot as plt
+import cv2
+import os
 import torch
-from torchvision import models, transforms
 from PIL import Image
+import numpy as np
+from torch.utils.data import Dataset
+from pycocotools.coco import COCO
 
 device = torch.device("cpu")
 
+def clear():
+    os.system("cls")
 
+def dataset_mode():
+    clear()
+    command = input("""
+s - select image
+e - exit
+Enter command: """)
+    if command == "s":
+        select_image()
+    elif command == "e":
+        exit()
+    else:
+        print("Invalid command")
+
+def select_image():
+    tk.Tk().withdraw()
+    root = tk.Tk()
+    root.attributes("-topmost", True) 
+    root.withdraw()
+    filename = askopenfilename(parent=root, initialdir="./dataset_images/actual_stuff/") # File selection dialog
+    root.destroy()
+    
+    for image in annotations["images"]:
+        if image["file_name"] == filename:
+            image_id = image["id"]
+            print("Image already exists in dataset")
+            break
+
+    annotations = json.load(open(r"dataset_images/actual_stuff/_annotations.coco.json"))
+    
+    im = cv2.imread(filename)
+    area = cv2.selectROI("Select area", im, fromCenter=False, showCrosshair=False)
+    
 def main():
-    resnet50 = models.resnet50(pretrained=True)
-
+    clear()
+    mode = input(""" 
+ds - dataset mode
+e - exit
+Enter mode: """)
+    if mode == "ds":
+        dataset_mode()
+    elif mode == "e":
+        exit()
+    else:
+        print("Invalid mode")
 
 if __name__ == "__main__":
     main()
